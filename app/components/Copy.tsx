d78e5ac95ea29b4b50eef9bcf7415c8ef261ce9d
Chico Sousa <ochicosousa@gmail.com>
2025-07-20 15:54:55 -0300

    refs

--- app/components/Copy.tsx
+++ app/components/Copy.tsx
@@ -3,8 +3,18 @@ import { gsap } from "gsap";
 
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { SplitText } from "gsap/SplitText";
-import React from "react";
+import React, { useRef } from "react";
+
+type SplitTextInstance = {
+  lines: HTMLElement[];
+  revert: () => void;
+};
 
 export default function Copy({ children }: { children: React.ReactNode }) {
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const elementRef = useRef<HTMLElement[]>([]);
+  const splitRef = useRef<SplitTextInstance[]>([]);
+  const lines = useRef<HTMLElement[]>([]);
+
   return React.cloneElement(children as React.ReactElement);
 }
