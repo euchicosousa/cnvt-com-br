f05dc33344c0bdac81a66db9b63d29048d707296
Chico Sousa <ochicosousa@gmail.com>
2025-07-20 15:33:11 -0300

    tentar

--- app/components/Copy.tsx
+++ app/components/Copy.tsx
@@ -4,8 +4,8 @@ import React, { useRef } from "react";
 import { useGSAP } from "@gsap/react";
 import { gsap } from "gsap";
 
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { SplitText } from "gsap/SplitText";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import SplitText from "gsap/SplitText";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
